0c212c40fb45139819db6faf561e5006935c8b4e
Ronald Seoh <ronaldseoh@icloud.com>
2021-03-08 16:03:38 -0500

    dataset_scripts/yelp_restaurants/dataset_test_NLI.py: positive-words.txt should be provided by pos_sentiment, not neg_sentiment; and vice versa

--- dataset_scripts/yelp_restaurants/dataset_test_NLI.py
+++ dataset_scripts/yelp_restaurants/dataset_test_NLI.py
@@ -5,8 +5,8 @@ yelp = datasets.load_dataset(
     data_files={
         'train': 'dataset_files/yelp_restaurants/yelp_academic_dataset_review.json',
         'restaurant_ids': 'dataset_files/yelp_restaurants/restaurantIDs.txt',
-        'pos_sentiment': 'dataset_scripts\opinion_lexicon\\negative_words.txt',
-        'neg_sentiment': 'dataset_scripts\opinion_lexicon\positive_words.txt'
+        'pos_sentiment': 'dataset_files/opinion_lexicon/positive-words.txt',
+        'neg_sentiment': 'dataset_files/opinion_lexicon/negative-words.txt'
     },
     cache_dir='./dataset_cache')
 
