5ace3a121cbf0056d67a0e07c961962ef94306ff
Ronald Seoh <ronaldseoh@icloud.com>
2021-03-12 23:54:54 -0500

    prompt_atsc_bert_amazon_electronics.ipynb: move the LR head into utils; save the best head based on validation loss

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,7 @@
 from .data_collator_smart_mlm import DataCollatorForSmartMLM
+from .multi_prompt_logistic_regression import MultiPromptSentimentClassificationHead
 
 __all__ = (
     DataCollatorForSmartMLM,
+    MultiPromptSentimentClassificationHead,
 )
