68eb2d7b4ce5a2b72e01f838554deb445a930d98
Ronald Seoh <ronaldseoh@icloud.com>
2021-03-14 10:29:23 -0400

    utils/prompt_output_head.py: filename change

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
 from .data_collator_smart_mlm import DataCollatorForSmartMLM
-from .prompt_logistic_regression import MultiPromptSentimentClassificationHead, SinglePromptLogitSentimentClassificationHead
+from .prompt_output_head import MultiPromptSentimentClassificationHead, SinglePromptLogitSentimentClassificationHead
 
 __all__ = (
     DataCollatorForSmartMLM,
