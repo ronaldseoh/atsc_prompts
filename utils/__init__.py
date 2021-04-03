68005064c5624d7aace0b83089809d993dd53f4f
Ronald Seoh <ronaldseoh@icloud.com>
2021-04-02 21:13:08 -0400

    utils/__init__.py: update

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,14 @@
 from .data_collator_smart_mlm import DataCollatorForSmartMLM
-from .prompt_output_head import MultiPromptSentimentClassificationHead, SinglePromptLogitSentimentClassificationHead, NoPromptSentimentClassificationHead
+from .prompt_output_head import (
+    MultiPromptSentimentClassificationHead,
+    SinglePromptLogitSentimentClassificationHead,
+    MultiPromptLogitSentimentClassificationHead,
+    NoPromptSentimentClassificationHead)
 
 __all__ = (
     DataCollatorForSmartMLM,
     MultiPromptSentimentClassificationHead,
     SinglePromptLogitSentimentClassificationHead,
+    MultiPromptLogitSentimentClassificationHead,
     NoPromptSentimentClassificationHead
 )
