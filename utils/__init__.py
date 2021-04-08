685249c9a410dc6061820782039ed03077ed322d
Ian Birle <ibirle@node040.cm.cluster>
2021-04-08 13:43:01 -0400

    Moved classifier to utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,10 @@
 from .data_collator_smart_mlm import DataCollatorForSmartMLM
-from .prompt_output_head import MultiPromptSentimentClassificationHead, SinglePromptLogitSentimentClassificationHead, NoPromptSentimentClassificationHead, MultiPromptNLISentimentClassificationHead
+from .prompt_output_head import MultiPromptSentimentClassificationHead, SinglePromptLogitSentimentClassificationHead, NoPromptSentimentClassificationHead, NLISentimentClassificationHead
 
 __all__ = (
     DataCollatorForSmartMLM,
     MultiPromptSentimentClassificationHead,
     SinglePromptLogitSentimentClassificationHead,
     NoPromptSentimentClassificationHead,
-    MultiPromptNLISentimentClassificationHead
+    NLISentimentClassificationHead
 )
