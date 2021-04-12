f736d43be0fcfe49047e72d2a139ab686867cff6
Ronald Seoh <ronaldseoh@icloud.com>
2021-04-12 17:17:35 -0400

    collect_results_snippet.py: minor fix

--- prompts_supervised_bert/collect_results_snippet.py
+++ prompts_supervised_bert/collect_results_snippet.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 
 # experiment id prefix
-experiment_id_prefix = 'bert_no_prompt_cls_lr_atsc'
+experiment_id_prefix = 'bert_prompt_logit_softmax_atsc'
 
 # Random seed
 random_seeds = [696, 685, 683, 682, 589]
@@ -16,8 +16,20 @@ random_seeds = [696, 685, 683, 682, 589]
 # path to pretrained MLM model folder or the string "bert-base-uncased"
 lm_model_paths = {
     'bert_amazon_electronics': '../progress/lm_further_pretraining_bert_amazon_electronics_bseoh_2021-03-06--18_59_53/results/checkpoint-1180388',
-    'bert-base-uncased': 'bert-base-uncased'
+    #'bert-base-uncased': 'bert-base-uncased'
 }
+
+# Prompts to be added to the end of each review text
+# Note: pseudo-labels for each prompt should be given in the order of (positive), (negative), (neutral)
+sentiment_prompts = {
+    'i_felt': {"prompt": "I felt the {aspect} was [MASK].", "labels": ["good", "bad", "ok"]},
+    'i_like': {"prompt": "I [MASK] the {aspect}.", "labels": ["like", "dislike", "ignore"]},
+    'made_me_feel': {"prompt": "The {aspect} made me feel [MASK].", "labels": ["good", "bad", "indifferent"]},
+    'the_aspect_is': {"prompt": "The {aspect} is [MASK].", "labels": ["good", "bad", "ok"]}
+}
+
+run_single_prompt = True
+run_multiple_prompts = True
 
 # Training settings
 training_domain = 'restaurants' # 'laptops', 'restaurants', 'joint'
@@ -31,9 +43,9 @@ if testing_domain != training_domain:
 else:
     cross_domain = False
 
-for config in tqdm.tqdm(itertools.product(lm_model_paths.keys())):
+for config in tqdm.tqdm(itertools.product(lm_model_paths.keys(), sentiment_prompts.keys())):
     
-    lm_model_name = config[0]
+    lm_model_name, prompt_key = config
     
     # We will use the following string ID to identify this particular (training) experiments
     # in directory paths and other settings
@@ -44,6 +56,8 @@ for config in tqdm.tqdm(itertools.product(lm_model_paths.keys())):
         experiment_id_config = experiment_id_config + 'cross_domain_'
 
     experiment_id_config = experiment_id_config + lm_model_name + '_'
+    experiment_id_config = experiment_id_config + 'single_prompt' + '_'
+    experiment_id_config = experiment_id_config + prompt_key + '_'
     
     test_metrics_all = []
 
