fb7cf70d62a399001ecf54a59799a59cb8bb52a7
Ronald Seoh <ronaldseoh@icloud.com>
2021-04-18 16:27:23 -0400

    prompts_supervised_bert: results update - logit_softmax_sum_probs, lr_sum (in-domain, further pretrained LMs only)

--- prompts_supervised_bert/collect_results_snippet.py
+++ prompts_supervised_bert/collect_results_snippet.py
@@ -10,13 +10,15 @@ import pandas as pd
 # experiment id prefix
 experiment_id_prefix = 'bert_prompt_logit_softmax_atsc'
 
+experiment_id_prefix_override = 'bert_prompt_logit_softmax_sum_probs_atsc'
+
 # Random seed
 random_seeds = [696, 685, 683, 682, 589]
 
 # path to pretrained MLM model folder or the string "bert-base-uncased"
 lm_model_paths = {
-    'bert_amazon_electronics': '../progress/lm_further_pretraining_bert_amazon_electronics_bseoh_2021-03-06--18_59_53/results/checkpoint-1180388',
-    'bert-base-uncased': 'bert-base-uncased'
+    'bert_yelp_restaurants': '../trained_models/lm_further_pretraining_bert_yelp_restaurants_bseoh_2021-03-22--15_03_31',
+    #'bert-base-uncased': 'bert-base-uncased'
 }
 
 # Prompts to be added to the end of each review text
@@ -31,12 +33,14 @@ sentiment_prompts = {
 run_single_prompt = True
 run_multiple_prompts = True
 
+prompts_merge_behavior = 'sum_probabilities'
+
 # Training settings
 training_domain = 'restaurants' # 'laptops', 'restaurants', 'joint'
 
 # Test settings
 testing_batch_size = 32
-testing_domain = 'laptops'
+testing_domain = 'restaurants'
 
 if testing_domain != training_domain:
     cross_domain = True
@@ -52,7 +56,7 @@ if run_single_prompt:
         
         # We will use the following string ID to identify this particular (training) experiments
         # in directory paths and other settings
-        experiment_id_config = experiment_id_prefix + '_'
+        experiment_id_config = experiment_id_prefix_override + '_'
         experiment_id_config = experiment_id_config + testing_domain + '_'
         
         if cross_domain:
@@ -71,7 +75,7 @@ if run_multiple_prompts:
         
         # We will use the following string ID to identify this particular (training) experiments
         # in directory paths and other settings
-        experiment_id_config = experiment_id_prefix + '_'
+        experiment_id_config = experiment_id_prefix_override + '_'
         experiment_id_config = experiment_id_config + testing_domain + '_'
         
         if cross_domain:
@@ -92,10 +96,10 @@ for prefix in experiment_id_prefix_complete:
         experiment_id = prefix + str(seed)
         
         # trained_models_prompts
-        test_metrics_ = os.path.join('..', 'trained_models_prompts', experiment_id)
+        test_metrics_ = os.path.join('..', 'trained_models', experiment_id)
         
         # Load test scores file
-        test_metrics = json.load(open(os.path.join('..', 'trained_models_prompts', experiment_id, 'test_metrics.json'), 'r'))
+        test_metrics = json.load(open(os.path.join('..', 'trained_models', experiment_id, 'test_metrics.json'), 'r'))
         
         test_metrics_all.append(test_metrics)
 
