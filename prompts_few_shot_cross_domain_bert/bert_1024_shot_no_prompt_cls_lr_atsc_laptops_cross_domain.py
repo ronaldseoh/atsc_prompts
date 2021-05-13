6d0415696b9235d4cc542b245555aa5b5380c6a2
Ronald Seoh <ronaldseoh@icloud.com>
2021-05-13 12:11:09 -0400

    prompts_few_shot_cross_domain_bert: added papermill scripts

---
+++ prompts_few_shot_cross_domain_bert/bert_1024_shot_no_prompt_cls_lr_atsc_laptops_cross_domain.py
@@ -0,0 +1,78 @@
+import os
+import itertools
+
+import papermill
+import tqdm
+
+
+# experiment id prefix
+experiment_id_prefix = 'bert_no_prompt_cls_lr_atsc'
+
+# Random seed
+random_seeds = [696, 685, 683, 682, 589]
+
+# path to pretrained MLM model folder or the string "bert-base-uncased"
+lm_model_paths = {
+    'bert_amazon_electronics': '../progress/lm_further_pretraining_bert_amazon_electronics_bseoh_2021-03-06--18_59_53/results/checkpoint-1180388',
+    #'bert-base-uncased': 'bert-base-uncased'
+}
+
+# Training settings
+training_domain = 'restaurants' # 'laptops', 'restaurants', 'joint'
+
+# Few-shot dataset size
+training_dataset_few_shot_size = 1024
+
+experiment_id_prefix_override = 'bert_' + str(training_dataset_few_shot_size) + '_shot_' + 'no_prompt_cls_lr_atsc'
+
+# Test settings
+testing_batch_size = 32
+testing_domain = 'laptops'
+
+if testing_domain != training_domain:
+    cross_domain = True
+else:
+    cross_domain = False
+
+# Results directory path
+results_path = 'results_' + experiment_id_prefix_override + '_' + testing_domain
+os.makedirs(results_path, exist_ok=True)
+
+# Run experiments
+print("Running experiments...")
+print()
+
+for seed, lm_model_name in tqdm.tqdm(itertools.product(random_seeds, lm_model_paths.keys())):
+    
+    # We will use the following string ID to identify this particular (training) experiments
+    # in directory paths and other settings
+    experiment_id = experiment_id_prefix_override + '_'
+    experiment_id = experiment_id + testing_domain + '_'
+    
+    if cross_domain:
+        experiment_id = experiment_id + 'cross_domain_'
+
+    experiment_id = experiment_id + lm_model_name + '_'
+    experiment_id = experiment_id + str(seed)
+    
+    print("Running experiment", experiment_id)
+    print()
+    
+    parameters_to_inject = {
+        'experiment_id': experiment_id,
+        'random_seed': seed,
+        'lm_model_path': lm_model_paths[lm_model_name],
+        'training_domain': training_domain,
+        'training_dataset_few_shot_size': training_dataset_few_shot_size,
+        'testing_batch_size': testing_batch_size,
+        'testing_domain': testing_domain
+    }
+
+    papermill.execute_notebook(
+       os.path.join('..', 'prompts_supervised_bert', experiment_id_prefix + '.ipynb'),
+       os.path.join(results_path, experiment_id + '.ipynb'),
+       parameters=parameters_to_inject,
+       log_output=True,
+       progress_bar=True,
+       autosave_cell_every=300  
+    )
